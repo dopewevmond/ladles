a41db04e0273414b9a7b007aec28ff7dae853ed1
Edmund Wevamwo <68214494+dopewevmond@users.noreply.github.com>
2021-08-17 02:45:47 +0000

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
-        'postgres://', 'postgresql://') or \
+    SQLALCHEMY_DATABASE_URI  = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'AGC789&ndbadsD'
